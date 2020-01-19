d7e67d2d5f8d2060a27e69f6df93d0277327f1c1
reckoner1429 <abhishekbisht1429@gmail.com>
2020-01-19 16:31:51 +0530

    added viewsets for the model classes

--- cycletracker/cycleissuer/views.py
+++ cycletracker/cycleissuer/views.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render
-from django.views. import View
+from rest_framework import viewsets
+from .models import User, Cycle
+from .serializers import UserSerializer, CycleSerializer
 # Create your views here.
 
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class CycleViewSet(viewsets.ModelViewSet):
+    queryset = Cycle.objects.all()
+    serializer_class = CycleSerializer
+    
